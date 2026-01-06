22b25dfbdaa127835aa5834264b36ef69fe660ef
Kenny Akanbi <kennyakanbi@gmail.com>
2026-01-06 21:07:38 +0100

    Fix

--- project/myapp/apps.py
+++ project/myapp/apps.py
@@ -1,25 +1,5 @@
-# myapp/apps.py
 from django.apps import AppConfig
-from django.conf import settings
 
 class MyappConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'myapp'
-
-    def ready(self):
-        # Only try this in production to avoid creating on every run
-        import django
-        from django.contrib.auth import get_user_model
-        from django.db.utils import OperationalError, ProgrammingError
-
-        User = get_user_model()
-        try:
-            if not User.objects.filter(username="admin").exists():
-                User.objects.create_superuser(
-                    username="admin",
-                    email="admin@example.com",
-                    password="Admin123!"
-                )
-        except (OperationalError, ProgrammingError):
-            # DB not ready yet (migrations not applied) — ignore
-            pass
