e96ac4777e6fa720b8d7d2248175fee895a63fb4
Kenny Akanbi <kennyakanbi@gmail.com>
2026-01-06 22:43:53 +0100

    Fix

--- project/myapp/urls.py
+++ project/myapp/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views  
 from django.views.generic import RedirectView
+from .views import check_superuser
 
 urlpatterns = [
     path('', views.home, name='home'),
@@ -21,4 +22,7 @@ urlpatterns += [
     path('services', RedirectView.as_view(url='/services/', permanent=True)),
     path('intern', RedirectView.as_view(url='/intern/', permanent=True)),
     path('blog', RedirectView.as_view(url='/blog/', permanent=True)),
+    path("check-superuser/", check_superuser),
 ]
+
+
