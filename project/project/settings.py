a9cb868e2f3c9d5798a2dcf2412541d2dfe180c9
Kenny Akanbi <kennyakanbi@gmail.com>
2026-01-06 14:39:54 +0100

    Fix render

--- project/project/settings.py
+++ project/project/settings.py
@@ -143,9 +143,9 @@ USE_TZ = True
 # =====================
 # STATIC FILES
 # =====================
+STATIC_ROOT = BASE_DIR / "staticfiles"
 STATIC_URL = "/static/"
 STATICFILES_DIRS = [BASE_DIR / "static"]
-STATIC_ROOT = BASE_DIR / "staticfiles"
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # =====================
--- project/start.sh
+++ project/start.sh
@@ -1,5 +1,4 @@
-#!/bin/bash
-python manage.py migrate
-python manage.py createsuperuser --noinput
-python manage.py collectstatic --noinput
+python manage.py migrate && \
+python manage.py createsuperuser --noinput && \
+python manage.py collectstatic --noinput && \
 gunicorn project.wsgi:application
