928d8ea5e93cfb29b2a17b5813bd5a1c1023ac78
Kenny Akanbi <kennyakanbi@gmail.com>
2026-01-07 11:22:52 +0100

    Fix

--- project/project/settings.py
+++ project/project/settings.py
@@ -51,7 +51,14 @@ else:
 # STATIC FILES
 # =====================
 STATIC_URL = "/static/"
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
+
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # =====================
 # SECURITY & HTTPS (Render)
