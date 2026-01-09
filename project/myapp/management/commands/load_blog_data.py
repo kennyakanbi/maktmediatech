1ad5b91884aea510a98006f65391e67a2e8c4ae6
Kenny Akanbi <kennyakanbi@gmail.com>
2026-01-09 11:13:11 +0100

    render deploy

--- project/myapp/management/commands/load_blog_data.py
+++ project/myapp/management/commands/load_blog_data.py
@@ -1,15 +1,14 @@
+# myapp/management/commands/load_blog_data.py
 from django.core.management.base import BaseCommand
 from django.core.management import call_command
-from django.db.utils import IntegrityError
+from myapp.models import Blog
 
 class Command(BaseCommand):
-    help = "Load blog fixture safely"
+    help = "Load blog fixture if no blogs exist"
 
     def handle(self, *args, **kwargs):
-        try:
-            call_command('loaddata', 'blog_data', verbosity=0)
-            self.stdout.write(self.style.SUCCESS('Blog data loaded'))
-        except IntegrityError:
-            self.stdout.write('Blog data already exists')
-        except Exception:
-            pass
+        if Blog.objects.exists():
+            self.stdout.write(self.style.SUCCESS("Blogs already loaded, skipping"))
+        else:
+            call_command('loaddata', 'myapp/fixtures/blog_data.json')
+            self.stdout.write(self.style.SUCCESS("Blog fixture loaded successfully"))
